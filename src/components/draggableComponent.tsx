9684c9a64be488d42a27a80d95cde6a87900f44e
kwarn <karlwarner.dev@gmail.com>
2024-06-13 10:27:28 +0100

    minimize functionality for add-note

--- src/components/draggableComponent.tsx
+++ src/components/draggableComponent.tsx
@@ -6,7 +6,7 @@ type Position = {
 };
 
 type DraggableComponentProps = {
-  isVisiable: Boolean;
+  isVisiable: boolean;
   children: ReactElement;
   toggleIsVisiable: () => void;
 };
@@ -20,15 +20,31 @@ const DraggableComponent = ({
   const [position, setPosition] = useState<Position>({ x: 300, y: 100 });
   const [dragging, setDragging] = useState<boolean>(false);
   const [rel, setRel] = useState<Position>({ x: 0, y: 0 });
+  const [isMinimized, setIsMinimized] = useState<boolean>(false);
+  const [lastPosition, setLastPosition] = useState<Position>({
+    x: 300,
+    y: 100,
+  });
 
-  const minimize = () => { setPosition({ x: 300, y: 100 }); }; //TODO: wip
+  const toggleMinimize = () => {
+    if (isMinimized) {
+      setPosition(lastPosition);
+      setIsMinimized(false);
+      return;
+    }
+    setIsMinimized(true);
+    setLastPosition(position);
 
+    const y = window.innerHeight - 25;
+    setPosition({ x: 0, y });
+  };
 
   useEffect(() => {
     setPosition({ x: 300, y: 100 });
   }, [isVisiable]);
 
   const onMouseDown = (e: React.MouseEvent<HTMLDivElement>): void => {
+    if (isMinimized) return toggleMinimize();
     if (e.button !== 0) return;
 
     const target = e.target as HTMLElement;
@@ -90,15 +106,27 @@ const DraggableComponent = ({
           left: position.x,
           top: position.y,
           minWidth: "200px",
-          minHeight: "200px",
+          minHeight: isMinimized ? "0 " : "200px",
+          position: isMinimized ? "fixed" : "absolute",
+          transition: !dragging ? "top 0.5s" : "none",
         }}
-        className="absolute cursor-move flex flex-col align-center bg-green-100 rounded-md select-none"
+        className="cursor-move flex flex-col align-center bg-white select-none border-2"
       >
-        <div className="bg-green-300 w-full flex flex-row top-0 justify-end rounded-md">
-          <div className="mr-2">_</div>
-          <div onClick={toggleIsVisiable} className="mr-2 cursor-pointer">X</div>
+        <div className="bg-gray-200 w-full flex flex-row top-0 justify-end">
+          <div className="mr-2" onClick={toggleMinimize}>
+            _
+          </div>
+          <div onClick={toggleIsVisiable} className="mr-2 cursor-pointer">
+            X
+          </div>
         </div>
-        <div className="flex-1 content-center justify-center align-middle">
+        <div
+          style={{
+            maxHeight: isMinimized ? "0px" : "auto",
+            overflow: isMinimized ? "hidden" : "visible",
+          }}
+          className="flex-1 content-center justify-center align-middle"
+        >
           {children}
         </div>
       </div>
